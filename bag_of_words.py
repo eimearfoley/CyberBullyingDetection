0fe9278845f5d54c81aab9f7d7449ed0f99e68fc
Eimear Foley <eimearfoley96@gmail.com>
2017-06-19 11:57:28 +0100

    update
    include pickling, test and train, feature scaling

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,11 +1,14 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import os
 import string
-import csv
+import pickle
 
 vectorizer = CountVectorizer()
-output = open('results.csv', 'w')
-writer = csv.writer(output, delimiter=';', quotechar='"')
+#output = open('results.csv', 'w')
+#writer = csv.writer(output, delimiter=';', quotechar='"')
+min_max_scaler = preprocessing.MinMaxScaler()
 
 if __name__ == '__main__':
     dataset_loc = '../Insight/2017/textpacket1'
@@ -24,18 +27,36 @@ if __name__ == '__main__':
                 texts.append(file_contents)
         vectorizer = CountVectorizer()
         bag_of_words = vectorizer.fit_transform(texts)
-        bag_of_words = bag_of_words.toarray()
-        data = [[name, bag_of_words, 'Y']]
-        writer.writerows(data)
-        i_loc = vectorizer.vocabulary_.get('and')
-        #print("and", i_loc)
-        owen_loc = vectorizer.vocabulary_.get('owen')
-        #print("owen", owen_loc)
-    #print(vectorizer.get_feature_names())
-    print(bag_of_words.shape)
+        bag_of_words = bag_of_words.toarray() # from scarse to numpy array
+        bag_of_words = min_max_scaler.fit_transform(bag_of_words) #feature scaling
+
+        # pickling of 2D array
+        pickle.dump(bag_of_words, open('results.pkl', 'wb'))
+        Z = pickle.load(open('results.pkl', 'rb'))
+
+        # splitting 2D array into test and train data
+        Z_train, Z_test = train_test_split(Z, test_size=0.25, random_state=42)
+        print(Z_train,'split' ,Z_test)
+
+        #print(bag_of_words)
+        #np.savetxt('npresults.csv', bag_of_words, delimiter=' ')
+    #words = vectorizer.get_feature_names()
+    #data = [['filename', [word for word in words], 'class']]
+    #writer.writerows(data)
+    # Sum up the counts of each vocabulary word
+    #dist = np.sum(bag_of_words, axis=0)
+
+    # For each, print the vocabulary word and the number of times it 
+    # appears in the training set
+    """for tag, count in zip(words, dist):
+        print(count, tag)
     for i, row in enumerate(bag_of_words):
         print(i, row)
         #print(row.get_feature_names())
            #if row[owen_loc] != 0:
                 #print(file[i])"""
-output.close()
+#output.close()
+
+
+
+
