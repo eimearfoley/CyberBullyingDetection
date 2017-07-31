ab19069975355c974e42c2bafc7c0edfd2870cbc
Eimear Foley <eimearfoley96@gmail.com>
2017-07-31 14:14:29 +0100

    Update plotting.py
    included plotting with matplotlib

--- plotting.py
+++ plotting.py
@@ -103,20 +103,18 @@ def make_tfidf(dct):
     y = [1 if dct[f] else 0 for f in dct]
     return X, y, svm_bow
 
-def train_test(X, y, svm_bow):
+def train_test(X, y, svm_bow, clas, classifier):
     print("Training and testing")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) # split data for train and test
-    clf ={'Linear SVM Classifier': LinearSVC()} # declare our classifiers
     print("\nClassifiers")
-    for clas in clf:
-        if clas == "SVM Classifier":
-            bag = svm_bow.toarray() # use normalized data for SVM classifier
-            bag_train, bag_test, y_train, y_test = train_test_split(bag, y, test_size=0.2)
-            clf[clas].fit(bag_train, y_train)
-            y_pred = clf[clas].predict(bag_test)
-        else:
-            clf[clas].fit(X_train, y_train)
-            y_pred = clf[clas].predict(X_test)
+    if clas == "SVM Classifier":
+        bag = svm_bow.toarray() # use normalized data for SVM classifier
+        bag_train, bag_test, y_train, y_test = train_test_split(bag, y, test_size=0.2)
+        classifier.fit(bag_train, y_train)
+        y_pred = clf[clas].predict(bag_test)
+    else:
+        classifier.fit(X_train, y_train)
+        y_pred = clf[clas].predict(X_test)
     return results(y_test, y_pred, clas)
 
 def results(y_test, y_pred, clas):
@@ -137,19 +135,48 @@ def results(y_test, y_pred, clas):
 
 if __name__ == "__main__":
     data = make_dataset()
-    k_scores = []
-    a_scores = []
-    n_range = []
-    for n in range(100, 3001, 100):
-        print("****NUMBER: %i ****" % n)
-        dct = make_samples(data, n)    
-        print("\n****BAG OF WORDS****\n")
-        X, y, svm_bow = make_bag_of_words(dct)
-        print("\n**** TRAIN & TEST BAG ****\n")
-        k, a = train_test(X,y, svm_bow)
-        k_scores += [k]
-        a_scores += [a]
-        n_range += [n]
-    plt.plot(n_range, k_scores, 'r--', n_range, a_scores, 'b--')
-    plt.show()
-             
+    clf ={'Linear SVM Classifier': LinearSVC(), 'RandomForest Classifier': RandomForestClassifier(), 'GaussianNB': GaussianNB(), 'Decision Tree': tree.DecisionTreeClassifier()}
+    for clas in clf:
+        print("CLASSIFIER: %s" % clas)
+        k_scores = []
+        a_scores = []
+        n_range = []
+        kt_s = []
+        at_s = []
+        for n in range(100, 3001, 100):
+            print("****NUMBER: %i ****" % n)
+            dct = make_samples(data, n)    
+            print("\n****BAG OF WORDS****\n")
+            X, y, svm_bow = make_bag_of_words(dct)
+            print("\n****TFIDF****\n")
+            X2, y2, svm_tfidf = make_tfidf(dct)
+            print("\n**** TRAIN & TEST BAG ****\n")
+            classifier = clf[clas]
+            k, a = train_test(X,y, svm_bow, clas, classifier)
+            print("\n**** TRAIN & TEST TFIDF ****\n")
+            kt, at = train_test(X2,y2, svm_tfidf, clas, classifier)
+            k_scores += [k]
+            a_scores += [a]
+            kt_s += [kt]
+            at_s += [at]
+            n_range += [n]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plt.title('%s' % clas)
+        plt.xlabel('Data Subsets')
+        plt.ylabel('Kappa & Accuracy Scores')
+        plt.plot(n_range, k_scores, 'ro-', label='Bag Kappa')
+        plt.plot(n_range, a_scores, 'bo-', label='Bag Accuracy')
+        plt.plot(n_range, kt_s, 'o-', label='TFIDF Kappa')
+        plt.plot(n_range, at_s, 'go-', label='TFIDF Accuracy')
+        plt.legend()
+        for xy in zip(n_range, k_scores):
+            ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, a_scores):
+            ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, kt_s):
+            ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, at_s):
+            ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        plt.grid()
+        plt.show()            
