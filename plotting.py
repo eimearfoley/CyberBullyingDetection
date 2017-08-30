bf08d1fcf75b7ecf7c19b77948056b14238906df
Eimear Foley <eimearfoley96@gmail.com>
2017-08-30 14:52:09 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -27,7 +27,7 @@ def make_dataset():
     bully2 =10
     bully3 = 13
     wrong = ['None', 'n/a', 'o', 'n/a0', "0`", "`0", 'N/a', 'N/A', 'n/A']
-    with open('formspring.csv') as csvfile:
+    with open('formspring1.csv') as csvfile:
         reader = csv.reader(csvfile)
         i=0
         for rows in reader:
@@ -118,6 +118,28 @@ def train_test(X, y, svm_bow, clas, classifier):
         y_pred = clf[clas].predict(X_test)
     return results(y_test, y_pred, clas)
 
+def k_fold(X, y, svm_bow, clas, classifier):
+    y = np.array(y)
+    skf = StratifiedKFold(n_splits=10)
+    if clas == "SVM Classifier":
+        bag = svm_bow.toarray() # use normalized data for SVM classifier
+        for train_index, test_index in skf.split(bag, y):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+    else:
+        for train_index, test_index in skf.split(X, y):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+    clf[clas].fit(X_train, y_train)
+    y_pred = clf[clas].predict(X_test)
+    return results(y_test, y_pred, clas)
+
+def cross_fold(X, y, svm_bow, clas, classifier):
+    if clas == "SVM Classifier":
+        X = svm_bow.toarray() # use normalized data for SVM classifier
+    scores = cross_val_score(classifier, X, y, cv=10, scoring='f1')
+    print("Average: %.2f" % (sum(scores) / float(len(scores))))
+
 def results(y_test, y_pred, clas):
     # print results and scores of all classifiers
     print("\n"+clas+" Report")
@@ -127,24 +149,33 @@ def results(y_test, y_pred, clas):
     print(cmtx)
     acc = (cmtx[0][0] + cmtx[1][1])/(cmtx[0][0] + cmtx[1][1] + cmtx[0][1] + cmtx[1][0])
     print('Accuracy: %.2f' % acc)
+    fn = cmtx[0][1]/(cmtx[0][1] + cmtx[1][0])
+    print('False Negative: %.2f' % fn)
     print("\nKappa Score:")
     kappa = cohen_kappa_score(y_test, y_pred)
     print('%.2f' % kappa)
     print('\nt-test: ')
     print(stats.ttest_ind(y_test, y_pred))
-    return kappa, acc
+    return kappa, acc, fn
 
 if __name__ == "__main__":
     data = make_dataset()
     clf ={'Linear SVM Classifier': LinearSVC(random_state=random_seed), 'RandomForest Classifier': RandomForestClassifier(random_state=random_seed), 'GaussianNB': GaussianNB(), 'Decision Tree': tree.DecisionTreeClassifier(random_state=random_seed)}
-    fig_num = 0
     for clas in clf:
         print("CLASSIFIER: %s" % clas)
+        false_tt_b = []
+        false_tt_t = []
+        false_kf_b = []
+        false_kf_t = []
         k_scores = []
         a_scores = []
         n_range = []
         kt_s = []
         at_s = []
+        kfb_ks = []
+        kfb_as = []
+        kft_ks = []
+        kft_as = []
         for n in range(100, 3001, 100):
             print("****NUMBER: %i ****" % n)
             dct = make_samples(data, n)    
@@ -154,24 +185,55 @@ if __name__ == "__main__":
             X2, y2, svm_tfidf = make_tfidf(dct)
             print("\n**** TRAIN & TEST BAG ****\n")
             classifier = clf[clas]
-            k, a = train_test(X,y, svm_bow, clas, classifier)
+            k, a, tbfn = train_test(X,y, svm_bow, clas, classifier)
             print("\n**** TRAIN & TEST TFIDF ****\n")
-            kt, at = train_test(X2,y2, svm_tfidf, clas, classifier)
+            kt, at, ttfn = train_test(X2,y2, svm_tfidf, clas, classifier)
+            print("\n*** CROSS FOLD VALIDATION ***\n")
+            print(cross_fold(X, y, svm_bow, clas, classifier))
+            print("\n**** K-FOLD BAG ****\n")
+            kfbk, kfba, kfbfn = k_fold(X, y, svm_bow, clas, classifier)
+            print("\n**** K-FOLD TFIDF ****\n")
+            kftk, kfta, kftfn = k_fold(X2, y2, svm_tfidf, clas, classifier)
+            false_tt_b += [tbfn]
+            false_tt_t += [ttfn]
+            false_kf_b += [kfbfn]
+            false_kf_t += [kftfn]
+            kfb_ks += [kfbk]
+            kfb_as += [kfba]
+            kft_ks += [kftk]
+            kft_as += [kfta]
             k_scores += [k]
             a_scores += [a]
             kt_s += [kt]
             at_s += [at]
             n_range += [n]
-        fig = plt.figure(fig_num)
-        #subplot is the figure name
-        ax = fig.add_subplot(111)
+        fig, ax = plt.subplots(nrows=3, ncols=1)
+        ax = fig.add_subplot(311)
         plt.title('%s' % clas)
         plt.xlabel('Data Subsets')
         plt.ylabel('Kappa & Accuracy Scores')
-        plt.plot(n_range, k_scores, 'ro-', label='Bag Kappa')
-        plt.plot(n_range, a_scores, 'bo-', label='Bag Accuracy')
-        plt.plot(n_range, kt_s, 'mo-', label='TFIDF Kappa')
-        plt.plot(n_range, at_s, 'go-', label='TFIDF Accuracy')
+        plt.plot(n_range, k_scores, color='red', linestyle='solid', label='Bag Kappa')
+        plt.plot(n_range, a_scores, color='blue', linestyle='solid', label='Bag Accuracy')
+        plt.plot(n_range, kt_s, color='green', linestyle='solid', label='TFIDF Kappa')
+        plt.plot(n_range, at_s, color='yellow', linestyle='solid', label='TFIDF Accuracy')
+        plt.legend()
+        ax2 = fig.add_subplot(312)
+        plt.title('%s' % clas)
+        plt.xlabel('Data Subsets')
+        plt.ylabel('Kappa & Accuracy Scores')
+        plt.plot(n_range, kfb_ks, 'ro-', label='K-Fold Bag Kappa')
+        plt.plot(n_range, kfb_as, 'bo-',  label='K-Fold Bag Accuracy')
+        plt.plot(n_range, kft_ks, 'go-', label='K-Fold TFIDF Kappa')
+        plt.plot(n_range, kft_as, 'yo-',  label='K-Fold TFIDF Accuracy')
+        plt.legend()
+        ax3 = fig.add_subplot(313)
+        plt.title('%s' % clas)
+        plt.xlabel('Data Subsets')
+        plt.ylabel('False Negative Accuracy')
+        plt.plot(n_range, false_tt_b, 'ro-', label='TT Bag Fn')
+        plt.plot(n_range, false_kf_b, 'co-', label='K-Fold Bag Fn')
+        plt.plot(n_range, false_tt_t, 'mo-', label='TT TFIDF Fn')
+        plt.plot(n_range, false_kf_t, 'o-', label='K-Fold TFIDF Fn')
         plt.legend()
         for xy in zip(n_range, k_scores):
             ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
@@ -181,6 +243,21 @@ if __name__ == "__main__":
             ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
         for xy in zip(n_range, at_s):
             ax.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, kfb_ks):
+            ax2.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, kfb_as):
+            ax2.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, kft_ks):
+            ax2.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, kft_as):
+            ax2.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, false_tt_b):
+            ax3.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, false_kf_b):
+            ax3.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, false_tt_t):
+            ax3.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
+        for xy in zip(n_range, false_kf_t):
+            ax3.annotate('%.2f' % xy[1], xy=xy, textcoords='data')
         plt.grid()
-        fig_num+=1
     plt.show()
